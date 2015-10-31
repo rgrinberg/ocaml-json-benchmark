a752e9039d2591013b53dbe47e75252cd0467413
Andy Myers <acm@dullroar.org>
2015-10-30 21:30:25 -0400

    update to build with ezjsonm 0.4.2

--- lib/bench_read.ml
+++ lib/bench_read.ml
@@ -5,7 +5,8 @@ let random_json =
   let random_events =
     1_000
     |> List.init ~f:(fun _ -> Random_data.random_event ())
-    |> List.map ~f:Random_data.Ez.to_json in
+    |> List.map ~f:Random_data.Ez.to_json  
+  in
   (`A random_events) |> Ezjsonm.to_string
 
 let () =
@@ -21,16 +22,20 @@ let () =
     (*   random_json |> Yojson.Safe.from_string |> ignore *)
     (* ); *)
     Bench.Test.create ~name:"ezjsonm read" (fun () ->
-      random_json
-      |> Ezjsonm.from_string
-      |> Ezjsonm.get_list Random_data.Ez.of_json
-      |> ignore
+      let event_list : Random_data.event list =
+	random_json
+        |> Ezjsonm.from_string
+        |> Ezjsonm.get_list Random_data.Ez.of_json
+      in
+      ignore event_list
     );
     Bench.Test.create ~name:"yojson read" (fun () ->
-      random_json
-      |> Yojson.Basic.from_string
-      |> Yojson.Basic.Util.to_list
-      |> List.map ~f:Random_data.Yo.of_json
-      |> ignore
+      let event_list : Random_data.event list =
+        random_json
+        |> Yojson.Basic.from_string
+        |> Yojson.Basic.Util.to_list
+        |> List.map ~f:Random_data.Yo.of_json
+      in
+      ignore event_list
     );
   ]
